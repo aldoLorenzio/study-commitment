d2d63c39c170d4e1a28688c8f61ec16631538d0e
Aldo Lorenzio <aldolorenzio@gmail.com>
2026-03-14 15:24:13 +0700

    test: initialization test and first test

---
+++ test/StudyCommitmentTest.ts
@@ -0,0 +1,33 @@
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
+import { network } from "hardhat";
+import { parseEther } from "viem";
+
+describe("Study commitment", async() =>{
+    async function deploy(){
+        const { viem } = await network.connect();
+        const [owner, student] = await viem.getWalletClients();
+
+        const study = await viem.deployContract("StudyCommitment",
+            [owner.account.address],
+            {client: {wallet: student} }
+        );
+        return {viem, study, owner, student};
+    }
+
+    it("should create a session with a ETH", async () =>{
+        const { study, student } = await deploy();
+
+        await study.write.createSession([1800n], {
+            value: parseEther("0.01"),
+            account: student.account
+        });
+
+        const session = await study.read.getSession([0n]);
+
+        assert.equal(session.stakedAmount, parseEther("0.01"));
+        assert.equal(session.currentStatus, 1); // 1 ini refer di enum index
+        assert.equal(session.student.toLowerCase(), student.account.address.toLowerCase());
+    });
+
+})
