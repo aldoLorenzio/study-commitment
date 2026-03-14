db5d4dd489b477b0c637d2d98dc78461cfa6feb7
Aldo Lorenzio <aldolorenzio@gmail.com>
2026-03-14 15:33:28 +0700

    fix: naming variable confusion

--- test/StudyCommitmentTest.ts
+++ test/StudyCommitmentTest.ts
@@ -6,42 +6,44 @@ import { parseEther } from "viem";
 describe("Study commitment", async() =>{
     async function deploy(){
         const { viem } = await network.connect();
-        const [owner, student] = await viem.getWalletClients();
+        const [deployer, randomAccount, charityAccount] = await viem.getWalletClients();
 
         const study = await viem.deployContract("StudyCommitment",
-            [owner.account.address],
-            {client: {wallet: student} }
+            [charityAccount.account.address],
+            {client: {wallet: deployer} }
         );
-        return {viem, study, owner, student};
+        return {viem, study, deployer, randomAccount, charityAccount};
     }
 
     it("should create a session with a ETH", async () =>{
-        const { study, student } = await deploy();
+        const { study, deployer } = await deploy();
 
         await study.write.createSession([1800n], {
             value: parseEther("0.01"),
-            account: student.account
+            account: deployer.account
         });
 
         const session = await study.read.getSession([0n]);
 
         assert.equal(session.stakedAmount, parseEther("0.01"));
         assert.equal(session.currentStatus, 1); // 1 ini refer di enum index
-        assert.equal(session.student.toLowerCase(), student.account.address.toLowerCase());
+        assert.equal(session.student.toLowerCase(), deployer.account.address.toLowerCase());
     });
 
     it("should reject if non creator tries to complete session", async() =>{
-        const { study, student, owner } = await deploy();
+        const { study, deployer, randomAccount } = await deploy();
         await study.write.createSession([1800n], {
             value: parseEther("0.01"),
-            account:student.account});
+            account:deployer.account});
 
         await assert.rejects(
-            () => study.write.completeSession([0n], { account: owner.account} ),
+            () => study.write.completeSession([0n], { account: randomAccount.account} ),
             (err: any ) => {
                 assert.ok(err.message.includes("Only creator can mark complete!"));
                 return true;
             }
         )
     });
+
+
 })
